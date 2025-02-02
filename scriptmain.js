662b0f98ace94033ba3156c40f587c212c84a9fe
rprethi <2271821@cmontmorency.qc.ca>
2025-02-02 16:33:33 -0500

    Revert "init"
    This reverts commit 93e86b466cd90fff630e286b985958fefb148d11.

--- scriptmain.js
+++ scriptmain.js
@@ -1,7 +1,7 @@
 /*self typing accueil*/
 
 var typed = new Typed(".auto-typed", {
-  strings: ["le développement web", "la modélisation 3D", "la programmation"],
+  strings: ["web development", "web design", "programming"],
   typeSpeed: 70,
   backSpeed: 70,
   loop: true,
